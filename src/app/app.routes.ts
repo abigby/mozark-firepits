67d1aca71dca6711a6da80aa0adde2cb3d152270
Andrew Bigby <abigby@barcodesinc.com>
2024-05-12 05:28:35 -0400

    added favicon.ico file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { 
         path: 'home', 
-        title:"Home", 
+        title:"Mozark Fire Pits", 
         loadComponent:() => import('./components/home/home.component').then(
             (m) => m.HomeComponent
         )
